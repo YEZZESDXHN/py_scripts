44ac9f14b27287fe075da43975f4d8ae4367e9eb
wzc <1234@valeo.com>
2024-04-29 16:10:15 +0800

    正常建立通信

--- py_canoe/py_canoe.py
+++ py_canoe/py_canoe.py
@@ -1,7 +1,9 @@
 import sys
+import time
 
 import can
 from PyQt5 import uic
+from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QWidget
 from can.interfaces.vector import VectorBus
 from udsoncan.connections import PythonIsoTpConnection
@@ -176,6 +178,35 @@ class VectorCanParamsd(dict):
     output_mode: can.interfaces.vector.xldefine.XL_OutputMode
 
 
+class busThread(QThread):
+    start_bus = pyqtSignal()
+
+    def __init__(self, app_name, channel_index, vectorAvailableConfigs_index, fd, vectorChannelCanParams,isotp_params):
+        super().__init__()
+        self._appname = app_name
+        self._channelindex = channel_index
+        self._vectorAvailableConfigsindex = vectorAvailableConfigs_index
+        self._fd = fd
+        self._vectorChannelCanParams = vectorChannelCanParams
+        self._isotp_params = isotp_params
+
+    def run(self):
+        print("busThread(QThread):线程")
+        VectorBus.set_application_config(app_name=self._appname, app_channel=self._channelindex,
+                                         **self._vectorAvailableConfigsindex)
+        self.canbus = VectorBus(channel=self._channelindex, app_name=self._appname, fd=bool(self._fd), **self._vectorChannelCanParams)
+        # uds_config = udsoncan.configs.default_client_config.copy()
+        canlister = can.Printer()
+        notifier = can.Notifier(self.canbus, [])  # Add a debug listener that print all messages
+        # self.tp_addr = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=0x123,
+        #                         rxid=0x456)  # Network layer addressing scheme
+        self.tp_addr = isotp.Address(isotp.AddressingMode.Normal_29bits, txid=0x18DA05F1, rxid=0x18DAF105,functional_id=0x18DB33F1)
+        # # stack = isotp.CanStack(bus=bus, address=tp_addr, params=isotp_params)              # isotp v1.x has no notifier support
+        stack = isotp.NotifierBasedCanStack(bus=self.canbus, notifier=notifier, address=self.tp_addr, params=self._isotp_params)
+        # self.conn = PythonIsoTpConnection(stack)
+        # self.conn.open()
+
+
 class MainWindows(QWidget):
 
     def __init__(self):
@@ -187,14 +218,12 @@ class MainWindows(QWidget):
         self.vectorAvailableConfigs = None
         self.ui = None
         self.vectorBusType = None  # 0:CAN2.2  1: CANFD
-        self.is_start=0
-        self.channel_index=None
+        self.is_start = 0
+        self.channel_index = None
         self.appName = 'uds_tool'
 
         self.ui = uic.loadUi("py_can_tool.ui")
         self.init_vector()
-
-
 
     def init_vector(self):
 
@@ -218,32 +247,38 @@ class MainWindows(QWidget):
         )
 
         self.isotp_params = {
-                             'blocking_send': True,
-                             'stmin': 32,                            # Will request the sender to wait 32ms between consecutive frame. 0-127ms or 100-900ns with values from 0xF1-0xF9
-                             'blocksize': 8,                         # Request the sender to send 8 consecutives frames before sending a new flow control message
-                             'wftmax': 0,                            # Number of wait frame allowed before triggering an error
-                             'tx_data_length': 8,                    # Link layer (CAN layer) works with 8 byte payload (CAN 2.0)
-                             # Minimum length of CAN messages. When different from None, messages are padded to meet this length. Works with CAN 2.0 and CAN FD.
-                             'tx_data_min_length': None,
-                             'tx_padding': 0,                        # Will pad all transmitted CAN messages with byte 0x00.
-                             'rx_flowcontrol_timeout': 5000,         # Triggers a timeout if a flow control is awaited for more than 1000 milliseconds
-                             'rx_consecutive_frame_timeout': 1000,   # Triggers a timeout if a consecutive frame is awaited for more than 1000 milliseconds
-                             'override_receiver_stmin': 0,      # When sending, respect the stmin requirement of the receiver. If set to True, go as fast as possible.
-                             'max_frame_size': 4095,                 # Limit the size of receive frame.
-                             'can_fd': True,                        # Does not set the can_fd flag on the output CAN messages
-                             'bitrate_switch': False,                # Does not set the bitrate_switch flag on the output CAN messages
-                             'rate_limit_enable': False,             # Disable the rate limiter
-                             'rate_limit_max_bitrate': 1000000,      # Ignored when rate_limit_enable=False. Sets the max bitrate when rate_limit_enable=True
-                             'rate_limit_window_size': 0.2,          # Ignored when rate_limit_enable=False. Sets the averaging window size for bitrate calculation when rate_limit_enable=True
-                             'listen_mode': False,                   # Does not use the listen_mode which prevent transmission.
-                            }
+            'blocking_send': True,
+            'stmin': 32,
+            # Will request the sender to wait 32ms between consecutive frame. 0-127ms or 100-900ns with values from 0xF1-0xF9
+            'blocksize': 8,
+            # Request the sender to send 8 consecutives frames before sending a new flow control message
+            'wftmax': 0,  # Number of wait frame allowed before triggering an error
+            'tx_data_length': 8,  # Link layer (CAN layer) works with 8 byte payload (CAN 2.0)
+            # Minimum length of CAN messages. When different from None, messages are padded to meet this length. Works with CAN 2.0 and CAN FD.
+            'tx_data_min_length': None,
+            'tx_padding': 0,  # Will pad all transmitted CAN messages with byte 0x00.
+            'rx_flowcontrol_timeout': 5000,
+            # Triggers a timeout if a flow control is awaited for more than 1000 milliseconds
+            'rx_consecutive_frame_timeout': 1000,
+            # Triggers a timeout if a consecutive frame is awaited for more than 1000 milliseconds
+            'override_receiver_stmin': 0,
+            # When sending, respect the stmin requirement of the receiver. If set to True, go as fast as possible.
+            'max_frame_size': 4095,  # Limit the size of receive frame.
+            'can_fd': True,  # Does not set the can_fd flag on the output CAN messages
+            'bitrate_switch': False,  # Does not set the bitrate_switch flag on the output CAN messages
+            'rate_limit_enable': False,  # Disable the rate limiter
+            'rate_limit_max_bitrate': 1000000,
+            # Ignored when rate_limit_enable=False. Sets the max bitrate when rate_limit_enable=True
+            'rate_limit_window_size': 0.2,
+            # Ignored when rate_limit_enable=False. Sets the averaging window size for bitrate calculation when rate_limit_enable=True
+            'listen_mode': False,  # Does not use the listen_mode which prevent transmission.
+        }
 
     def init_ui(self):
 
         self.ui.comboBox.activated.connect(self.ui_update_bus_type)
         self.ui.comboBox_2.activated.connect(self.ui_update_bus_params)
         self.ui.pushButton.clicked.connect(self.bus_start)
-
 
         # self.ui.comboBox_2.activated.connect(self.set_CANFD_ui)
         self.ui_update_channel()
@@ -272,7 +307,7 @@ class MainWindows(QWidget):
             self.ui.comboBox_2.clear()
             self.ui.comboBox_2.addItem('CAN')
             self.ui.comboBox_2.addItem('CANFD')
-            self.chooseBusType=1
+            self.chooseBusType = 1
             self.ui.comboBox_2.setCurrentIndex(self.chooseBusType)
             # self.ui.comboBox_2.setCurrentIndex(self.vectorBusType)
             # print(self.vectorAvailableConfigs[choose_index]['vector_channel_config'])
@@ -338,26 +373,50 @@ class MainWindows(QWidget):
             self.ui.label_6.setVisible(False)
             self.ui.label_8.setVisible(False)
 
-
-
     def bus_start(self):
+        # if self.is_start == 0:
+        #     print('bus_start')
+        #     self.bus_thread = busThread(self.appName, self.channel_index,
+        #                                 self.vectorAvailableConfigs[self.channel_index], self.chooseBusType,
+        #                                 self.vectorChannelCanParams,
+        #                                 self.isotp_params)
+        #     self.bus_thread.start()
+            # self.is_start=1
+
+        # else:
+        #     self.bus_thread.conn.close()
+        #     self.bus_thread.canbus.shutdown()
+        #     self.is_start = 0
+
         if self.is_start == 0:
-            VectorBus.set_application_config(app_name=self.app_name, app_channel=0, **self.vectorAvailableConfigs[self.channel_index])
-            canbus = VectorBus(channel=0, app_name=self.app_name, fd=True, **self.vectorChannelCanParams)
+            VectorBus.set_application_config(app_name=self.appName, app_channel=self.channel_index, **self.vectorAvailableConfigs[self.channel_index])
+            self.canbus = VectorBus(channel=self.channel_index, app_name=self.appName, fd=bool(self.chooseBusType), **self.vectorChannelCanParams)
             # uds_config = udsoncan.configs.default_client_config.copy()
             canlister = can.Printer()
-            notifier = can.Notifier(canbus, [])  # Add a debug listener that print all messages
-            tp_addr = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=0x123,
-                                    rxid=0x456)  # Network layer addressing scheme
-            # tp_addr = isotp.Address(isotp.AddressingMode.Normal_29bits, txid=0x18DA05F1, rxid=0x18DAF105,functional_id=0x18DB33F1)
-            # stack = isotp.CanStack(bus=bus, address=tp_addr, params=isotp_params)              # isotp v1.x has no notifier support
-            stack = isotp.NotifierBasedCanStack(bus=canbus, notifier=notifier, address=tp_addr, params=self.isotp_params)
-            self.conn = PythonIsoTpConnection(stack)
+            self.notifier = can.Notifier(self.canbus, [])  # Add a debug listener that print all messages
+            # tp_addr = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=0x123,
+            #                         rxid=0x456)  # Network layer addressing scheme
+            tp_addr = isotp.Address(isotp.AddressingMode.Normal_29bits, txid=0x18DA05F1, rxid=0x18DAF105,functional_id=0x18DB33F1)
+            # # stack = isotp.CanStack(bus=bus, address=tp_addr, params=isotp_params)              # isotp v1.x has no notifier support
+            self.stack = isotp.NotifierBasedCanStack(bus=self.canbus, notifier=self.notifier, address=tp_addr, params=self.isotp_params)
+            self.conn = PythonIsoTpConnection(self.stack)
             self.conn.open()
-            self.is_start == 1
+            self.is_start = 1
+            print('打开')
+            self.resetCANparams()
+
+
+
         else:
-            self.is_start == 0
             self.conn.close()
+            self.stack.stop()
+            self.notifier.stop()
+            self.canbus.shutdown()
+            self.is_start = 0
+            print('关闭')
+    def resetCANparams(self):
+        self.vectorAvailableConfigs = VectorBus._detect_available_configs()
+        self.ui_update_bus_type()
 
 
 
